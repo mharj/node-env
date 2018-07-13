d762d1a0df07f4c7469384d2f008e97dcbf2f11b
Marko Harjula <marko.harjula@gmail.com>
2018-07-13 12:57:00 +0300

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,2 +1,4 @@
 echo "Hello"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mharj/node-env/master/.prettierrc" -OutFile ".prettierrc"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mharj/node-env/master/.eslintrc.js" -OutFile ".eslintrc.js"
+npm install eslint eslint-config-google eslint-plugin-jsx eslint-plugin-react --save-dev
